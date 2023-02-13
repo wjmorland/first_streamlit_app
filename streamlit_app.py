6f0a1915a1ca3e263bec4a13230b68216ee92d8f
wjmorland <80605813+wjmorland@users.noreply.github.com>
2023-02-13 12:14:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('My Mom\'s New Healthy Diner')
-streamlit.text(🥣 'Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
