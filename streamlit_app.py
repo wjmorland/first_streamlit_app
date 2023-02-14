9a402d78094835527d2075a94f4c519421c039e2
wjmorland <80605813+wjmorland@users.noreply.github.com>
2023-02-14 14:45:19 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,9 @@ streamlit.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.text("The fruit list contains:")
-streamlit.text(my_data_row)
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit list contains:")
+streamlit.dataframe(my_data_rows)
 
 
 ## Google Forms ID - 1A1enbY2g41lY2h4NoWySNVyXwLR0WJxd0-2i_O3LYUQ
