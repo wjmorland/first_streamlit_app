597403e04631df497a0736b05bc17d65bcdf7d64
wjmorland <80605813+wjmorland@users.noreply.github.com>
2023-02-14 14:42:16 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,13 @@ fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
 ## Google Forms ID - 1A1enbY2g41lY2h4NoWySNVyXwLR0WJxd0-2i_O3LYUQ
 ## FoodData Central API key - eLgugHDaVGeho9PHdfhOaDj6XyI5bZujsZAszSih
